81e0e5f7776aae4fdde7eaaf955b5127c338d1cf
YunYouJun <me@yunyoujun.cn>
2021-02-13 00:33:37 +0800

    :sparkles: update webhook listen push

--- webhook/index.js
+++ webhook/index.js
@@ -1,19 +1,29 @@
-const { exec } = require("shelljs");
+const shell = require("shelljs");
 
 const http = require("http");
 
 // install with: npm install @octokit/webhooks
 const { Webhooks } = require("@octokit/webhooks");
-const webhooks = new Webhooks({
+const handler = new Webhooks({
   secret: "el-psy-congroo",
 });
 
-webhooks.onAny(({ id, name, payload }) => {
-  console.log(name, "event received");
+handler.on("push", (event) => {
+  ctx.logger.info(
+    `Received a push event for ${event.payload.repository.name} to ${event.payload.ref}`
+  );
 
-  exec("git pull && yarn build");
+  // git pull repo
+  if (shell.exec("git pull").code !== 0) {
+    console.error("Git 拉取失败，请检查默认分支。");
+  } else {
+    console.info("安装依赖...");
+    shell.exec("yarn");
+    console.info("生成静态文件");
+    shell.exec("yarn build");
+  }
 });
 
 const port = 7777;
-http.createServer(webhooks.middleware).listen(port);
+http.createServer(handler.middleware).listen(port);
 console.log(`Webhook listen: http://localhost:${port}`);
